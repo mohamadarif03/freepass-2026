e01440479b7a547fa631cfe19c2e1dd8e0fda9b9
Mohamad Arif <125123688+mohamadarif03@users.noreply.github.com>
2026-02-03 08:37:55 +0700

    feat: admin seeder

---
+++ e-canteen/database/seeders/AdminSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\RoleEnum;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+   
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+            'role' => RoleEnum::ADMIN->value,
+        ]);
+    }
+}
